3850bc25f58e10c8f3861e19c6afbb03df8eac01
irena-dubkova <61350622+irena-dubkova@users.noreply.github.com>
2020-08-10 21:51:19 +0300

    end

--- script.js
+++ script.js
@@ -6,20 +6,28 @@ let shiftSize = 0;
 function sliderScroll(scrollSize) {
 
     const sliderElement = document.querySelector('.teachers-list');
-    sliderElement.style.margin = `0 0 0 ${scrollSize}px`;
+    sliderElement.style.left = `${scrollSize}px`;
 }
 
-function sliderScrollLeft() {
-
+function sliderScrollLeft(event) {
+    event.stopPropagation();
     shiftSize = shiftSize - 30;
     sliderScroll(shiftSize);
 }
 
-function sliderScrollRight() {
-
+function sliderScrollRight(event) {
+    event.stopPropagation();
     shiftSize = shiftSize + 30;
     sliderScroll(shiftSize);
 }
 
 prevButton.addEventListener('click', sliderScrollLeft);
 nextButton.addEventListener('click', sliderScrollRight);
+
+const spaceBetween = document.querySelector('.slider');
+
+function error1() {
+    alert('!!!');
+}
+
+spaceBetween.addEventListener('click', error1);
